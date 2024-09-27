4f7837813fec7e388b9d1c145eebb68101b0dc3d
AndreyBritvin <38183643+AndreyBritvin@users.noreply.github.com>
2024-09-27 23:28:07 +0300

    Added macros definitions

--- include/my_stack_protection.h
+++ include/my_stack_protection.h
@@ -1,4 +1,16 @@
 #ifndef MY_STACK_PROTECTION_H_
 #define MY_STACK_PROTECTION_H_
 
+#ifdef CANARY_PROTECTION
+#define CANARY_DBG(...) __VA_ARGS__
+#else
+#define CANARY_DBG(...)
+#endif
+
+#ifdef HASH_PROTECTION
+#define HASH_DBG(...) __VA_ARGS__
+#else
+#define HASH_DBG(...)
+#endif
+
 #endif // MY_STACK_PROTECTION_H_
